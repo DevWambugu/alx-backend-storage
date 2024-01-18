5aa1cfe826a8398bee9fa9d31af073f2c415cf2d
DevWambugu <venusdata100@gmail.com>
2024-01-18 20:54:21 +0000

    use coalesce keyword

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -2,7 +2,7 @@
 -- Column names must be: band_name and lifespan (in years until 2022 - please use 2022 instead of YEAR(CURDATE()))
 -- You should use attributes formed and split for computing the lifespan
 --Your script can be executed on any database
-SELECT band_name, (IFNULL(split, '2022') - formed) AS lifespan
-    FROM metal_bands
-    WHERE style LIKE '%Glam rock%'
-    ORDER BY lifespan DESC;
+SELECT band_name, COALESCE(split, '2022') - formed) AS lifespan
+  FROM  metal_bands
+  WHERE  style LIKE '%Glam rock%'
+  ORDER BY lifespan DESC;
