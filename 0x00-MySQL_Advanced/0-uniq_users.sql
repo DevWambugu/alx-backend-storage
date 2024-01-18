0335acbac6c991478017efbe11c4d056155e6ff4
DevWambugu <venusdata100@gmail.com>
2024-01-18 20:05:26 +0300

    First commit

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,7 @@
+-- Createas a table called users
+-- The script can be executed on any database
+CREATE TABLE users (
+	id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+	name VARCHAR(255),
+	email VARCHAR(255) NOT NULL UNIQUE
+);
