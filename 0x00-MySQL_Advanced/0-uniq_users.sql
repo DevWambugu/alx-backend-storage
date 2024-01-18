1673b21a9112270ae767e726370b7caf63dea8d1
DevWambugu <venusdata100@gmail.com>
2024-01-18 20:06:52 +0300

    the name is not null

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -2,6 +2,6 @@
 -- The script can be executed on any database
 CREATE TABLE users (
 	id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-	name VARCHAR(255),
+	name VARCHAR(255) NOT NULL,
 	email VARCHAR(255) NOT NULL UNIQUE
 );
