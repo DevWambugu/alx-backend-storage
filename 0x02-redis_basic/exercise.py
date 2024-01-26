e99c1cb463d2446560d874996125fc7a1381097c
DevWambugu <venusdata100@gmail.com>
2024-01-26 18:26:02 +0300

    add methods

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 '''exercise.py'''
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable
 
 
 class Cache:
@@ -19,3 +19,26 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    '''union = Union[str, bytes, int, float] = None'''
+    '''fn_ = Callable[[Union[str, bytes, int, float]], union]'''
+
+    def get(self, key: str, fn: Callable[[Union[str, bytes, int, float]], Union[str, bytes, int, float]] = None) -> Union[str, bytes, int, float]:
+        ''' This function create a get method that
+        take a key string argument and an optional
+        Callable argument named fn. This callable will be
+        used to convert the data back to the desired format.'''
+        data = self._redis.get(key)
+        if data is not None and fn is not None:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> str:
+        '''This function automatically parametrize
+        Cache.get with the correct conversion function'''
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> int:
+        '''This function also automatically parametrize
+        Cache.get with the correct conversion function'''
+        return self.get(key, fn=int)
